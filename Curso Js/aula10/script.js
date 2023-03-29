d264eebeb94f716c78296fb74ca99d9dffb42e0e
Lsacerdote <lucassacerdote227@gmail.com>
2023-03-29 17:04:29 -0300

    aula 10 p1

---
+++ Curso Js/aula10/script.js
@@ -0,0 +1,14 @@
+//  Ariméticos 
+ // + Adição / Concatenação
+ // - / *  Subtração / Mutiplicação
+ //** potenciação
+ // % resto da divição
+const num1 = 2;
+const num2 = 10;
+console.log(num1 ** num2);
+
+let contador = 1;
+contador++; //2
+contador++; //3
+console.log(contador);
+
